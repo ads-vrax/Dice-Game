ae6b7bef144ec6c466b65e84d60dd759dc0d4302
ads-vrax <106771456+ads-vrax@users.noreply.github.com>
2025-02-17 14:31:47 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+var diceOneImage = "images/dice" + randomNumber1 + ".png";
+var diceTwoImage = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", diceOneImage);
+document.querySelector(".img2").setAttribute("src", diceTwoImage);
+        
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
